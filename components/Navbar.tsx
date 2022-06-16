1ca302c0bda42731a963aa7f559f0bb79672ee7f
rodrisapiens <rniveyroreguero@frba.utn.edu.ar>
2022-06-16 13:03:36 -0300

    sign in and sign up

---
+++ components/Navbar.tsx
@@ -0,0 +1,56 @@
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+import React from "react";
+
+interface NavProps {
+  navigation: any;
+  title: string;
+}
+
+export const Navbar = ({ navigation, title }: NavProps) => {
+  return (
+    <>
+      <View style={styles.nav}>
+        <TouchableOpacity
+          style={styles.backBtn}
+          onPress={() => navigation.goBack()}
+        >
+          <Image
+            resizeMode="contain"
+            source={require("./../assets/arrowleft.png")}
+          />
+          <Text style={styles.back}>Back</Text>
+        </TouchableOpacity>
+        <Text style={styles.title}>{title}</Text>
+      </View>
+    </>
+  );
+};
+
+export default Navbar;
+
+const styles = StyleSheet.create({
+  back: {
+    padding: 16,
+    borderRadius: 10,
+    color: "#fff",
+  },
+  nav: {
+    flexDirection: "row",
+    marginTop: "10%",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    width: "100%",
+  },
+  backBtn: {
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    zIndex: 2,
+  },
+  title: {
+    color: "#fff",
+    position: "absolute",
+    width: "100%",
+    textAlign: "center",
+  },
+});
